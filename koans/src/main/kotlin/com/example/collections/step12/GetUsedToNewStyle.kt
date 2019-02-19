3ef5f6b615658f1a0b1f19f014adf9b49ac08f11
Shinya Hijiri <shinya.hijiri@gmail.com>
2019-02-19 23:49:49 +0900

    Collections - Get used to new style

---
+++ koans/src/main/kotlin/com/example/collections/step12/GetUsedToNewStyle.kt
@@ -0,0 +1,10 @@
+package com.example.collections.step12
+
+fun doSomethingStrangeWithCollection(collection: Collection<String>): Collection<String>? {
+
+    val groupsByLength = collection. groupBy { s -> s.length }
+
+    val maximumSizeOfGroup = groupsByLength.values.map { group -> group.size }.max()
+
+    return groupsByLength.values.firstOrNull { group -> group.size == maximumSizeOfGroup }
+}
