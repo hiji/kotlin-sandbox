fea0aeefd6606fa97e2d56bdd2ca599fbbe28a50
Shinya Hijiri <shinya.hijiri@gmail.com>
2019-02-25 23:44:39 +0900

    Properties - Delegate how it works

---
+++ koans/src/main/kotlin/com/example/properties/step4/MyDate.kt
@@ -0,0 +1,19 @@
+package com.example.properties.step4
+
+import java.util.*
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+
+fun MyDate.toMillis(): Long {
+    val c = Calendar.getInstance()
+    c.set(year, month, dayOfMonth, 0, 0, 0)
+    c.set(Calendar.MILLISECOND, 0)
+    return c.timeInMillis
+}
+
+fun Long.toDate(): MyDate {
+    val c = Calendar.getInstance()
+    c.setTimeInMillis(this)
+    return MyDate(c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DATE))
+
+}
