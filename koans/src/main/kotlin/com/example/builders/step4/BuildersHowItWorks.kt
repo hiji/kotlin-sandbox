5c96d52f3d881007438a5ff87e83bb9d38df9313
Shinya Hijiri <shinya.hijiri@gmail.com>
2019-03-02 11:24:53 +0900

    Builders - Builders how it works

---
+++ koans/src/main/kotlin/com/example/builders/step4/BuildersHowItWorks.kt
@@ -0,0 +1,10 @@
+package com.example.builders.step4
+
+import com.example.builders.step4.Answer.b
+import com.example.builders.step4.Answer.c
+
+enum class Answer { a, b, c }
+
+val answers = mapOf<Int, Answer?>(
+        1 to c, 2 to b, 3 to b, 4 to c
+)
